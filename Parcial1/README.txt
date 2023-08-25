73918a615a0757be43fde0acf41b2cedf94676fc
jeanpierec <73913711+jeanpierec@users.noreply.github.com>
2023-08-24 22:24:32 -0500

    Archivos aprcial 1

---
+++ Parcial1/README.txt
@@ -0,0 +1,7 @@
+# Parcial
+
+Se adjuntan:
+
+1. Vagranfile: Para cada una de las máuinas nodos y servidor con aprovisionamiento por Shell.
+2. Scripts SH: Para el aprovisionamiento de Consul y aplicación NodeJS para cada máquina nodo.
+3. Archivos YAML: Para la prueba de dos esecanrios en Artillery.
---
+++ Parcial1/scriptDos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Inicio instalación Consul en el nodo"
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "Instalación Consul en el nodo"
+sudo apt update && sudo apt install -y consul
+echo "Se inicia el consul"
+consul agent -ui -dev -bind=192.168.100.4 -client=0.0.0.0 -data-dir=. > consul.log 2>&1 &
+echo "Instalacion de nodejs"
+sudo apt install -y nodejs
+node -v
+echo "Instalacion de npm"
+sudo apt install -y npm
+npm -v
+echo "Clone del GitHub"
+git clone https://github.com/omondragon/consulService
+cd consulService/app
+npm install consul
+npm install express
+node index.js 3000
